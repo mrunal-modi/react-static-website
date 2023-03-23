1017af84917232fa3c9082a3920bbe70ce9fc7da
mrunal-modi <mrunal.modi@gmail.com>
2023-03-23 19:54:43 +1100

    our-team-config loop

--- src/components/our-team/our-team.jsx
+++ src/components/our-team/our-team.jsx
@@ -1,36 +1,21 @@
 import "./our-team.scss";
-import UserProfilePic from "../../components/UserProfilePic/UserProfilePic"; 
-import { 
-    ImageTeamMember1, 
-    ImageTeamMember2,
-    ImageTeamMember3, 
-    FullNameTeamMember1, 
-    FullNameTeamMember2, 
-    FullNameTeamMember3, 
-    RoleTeamMember1, 
-    RoleTeamMember2,
-    RoleTeamMember3 
+import UserProfilePic from "../../components/UserProfilePic/UserProfilePic";
+import {
+    teamInfo
 } from "../../config/our-team-config";
 
 
-const OurTeam = ({className}) => {
+const OurTeam = ({ className }) => {
     return (
         <div className="team" id="team">
-            <UserProfilePic
-                src={ImageTeamMember1}
-                fullName={FullNameTeamMember1}
-                designation={RoleTeamMember1}
-            />
-            <UserProfilePic
-                src={ImageTeamMember2}
-                fullName={FullNameTeamMember2}
-                designation={RoleTeamMember2}
-            />
-            <UserProfilePic
-                src={ImageTeamMember3}
-                fullName={FullNameTeamMember3}
-                designation={RoleTeamMember3}
-            />
+
+            {teamInfo.map((el, i) => (
+                <UserProfilePic key={i}
+                    src={el.image}
+                    fullName={el.fullName}
+                    designation={el.role} />
+
+            ))}
         </div>
     )
 }
--- src/config/our-team-config.js
+++ src/config/our-team-config.js
@@ -1,11 +1,17 @@
-export const ImageTeamMember1 = "/images/ImageTeamMember1.jpeg";
-export const ImageTeamMember2 = "/images/ImageTeamMember2.jpeg";
-export const ImageTeamMember3 = "/images/logo.png";
-
-export const FullNameTeamMember1 = "Mrunal Modi";
-export const FullNameTeamMember2 = "Lalit Sharma";
-export const FullNameTeamMember3 = "You";
-
-export const RoleTeamMember1 = "Founder | Student";
-export const RoleTeamMember2 = "Founder | Tutor";
-export const RoleTeamMember3 = "You | Student";
+export const teamInfo = [
+    {
+        image: "/images/ImageTeamMember1.jpeg",
+        fullName: "Mrunal Modi",
+        role: "Founder | Student"
+      },
+      {
+        image: "/images/ImageTeamMember2.jpeg",
+        fullName: "Lalit Sharma",
+        role: "Founder | Student"
+      },
+      {
+        image: "/images/logo.png",
+        fullName: "You",
+        role: "You | Student"
+      }
+    ]
